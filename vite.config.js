d5272ea09450832aa2c56dc8b9e333772ba07be8
YangDoo <2087058585@qq.com>
2025-08-02 17:31:44 +0800

    elementPlus自动按需引入配置

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,20 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+// elementPlus按需导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
     vue(),
     vueDevTools(),
   ],
