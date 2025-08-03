2105a5f7ac3aa6f78596f0b98a890a591484404c
YangDoo <2087058585@qq.com>
2025-08-03 18:13:40 +0800

    吸顶导航交互实现

---
+++ src/views/Layout/components/LayoutFixed.vue
@@ -0,0 +1,96 @@
+<script setup>
+// VueUse
+import { useScroll } from '@vueuse/core'
+const { y } = useScroll(window)
+</script>
+
+<template>
+  <div class="app-header-sticky" :class="{ show: y > 128 }">
+    <div class="container">
+      <RouterLink class="logo" to="/" />
+      <!-- 导航区域 -->
+      <ul class="app-header-nav">
+        <li class="home"><RouterLink to="/">首页</RouterLink></li>
+        <li class="home"><RouterLink to="/">居家</RouterLink></li>
+        <li class="home"><RouterLink to="/">美食</RouterLink></li>
+        <li class="home"><RouterLink to="/">服饰</RouterLink></li>
+        <li class="home"><RouterLink to="/">母婴</RouterLink></li>
+        <li class="home"><RouterLink to="/">个护</RouterLink></li>
+        <li class="home"><RouterLink to="/">严选</RouterLink></li>
+        <li class="home"><RouterLink to="/">数码</RouterLink></li>
+        <li class="home"><RouterLink to="/">运动</RouterLink></li>
+        <li class="home"><RouterLink to="/">杂项</RouterLink></li>
+      </ul>
+
+      <div class="right">
+        <RouterLink to="/">品牌</RouterLink>
+        <RouterLink to="/">专题</RouterLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style scoped lang='scss'>
+.app-header-sticky {
+  width: 100%;
+  height: 80px;
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: 999;
+  background-color: #fff;
+  border-bottom: 1px solid #e4e4e4;
+  // 此处为关键样式!!!
+  // 状态一：往上平移自身高度 + 完全透明
+  transform: translateY(-100%);
+  opacity: 0;
+
+  // 状态二：移除平移 + 完全不透明
+  &.show {
+    transition: all 0.3s linear;
+    transform: none;
+    opacity: 1;
+  }
+
+    .app-header-nav {
+      display: flex;
+      li{
+        margin: 0 18px;
+        font-size: 18px;
+        font-weight: bolder;
+      }
+    }
+
+  .container {
+    display: flex;
+    align-items: center;
+  }
+
+  .logo {
+    width: 200px;
+    height: 80px;
+    background: url("@/assets/images/logo.png") no-repeat right 2px;
+    background-size: 160px auto;
+  }
+
+  .right {
+    width: 220px;
+    display: flex;
+    text-align: center;
+    padding-left: 40px;
+    border-left: 2px solid $xtxColor;
+
+    a {
+      width: 38px;
+      margin-right: 40px;
+      font-size: 16px;
+      line-height: 1;
+
+      &:hover {
+        color: $xtxColor;
+      }
+    }
+  }
+}
+</style>
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -2,6 +2,7 @@
 import LayoutNav from './components/LayoutNav.vue';
 import LayoutHeader from './components/LayoutHeader.vue';
 import LayoutFooter from './components/LayoutFooter.vue';
+import LayoutFixed from './components/LayoutFixed.vue';
 
 </script>
 
@@ -10,4 +11,5 @@ import LayoutFooter from './components/LayoutFooter.vue';
     <LayoutHeader />
     <LayoutFooter />
     <RouterView />
+    <LayoutFixed />
 </template>
