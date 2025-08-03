1b772e27adad0a8c318cda03968d52fe30786ac3
YangDoo <2087058585@qq.com>
2025-08-03 15:20:52 +0800

    添加一二级路由

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
+import { RouterView } from 'vue-router';
+
 
 </script>
 
 <template>
+  <!-- 一级路由出口组件 -->
+  <RouterView />
   <el-button type="primary">Primary</el-button>
 </template>
 
