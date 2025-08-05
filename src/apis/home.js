5bbfdfc94c9424cdcc62df79e49153acec4f6827
YangDoo <2087058585@qq.com>
2025-08-05 21:38:11 +0800

    home-新鲜好物、人气推荐业务实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -6,3 +6,25 @@ export function getBannerAPI(){
         url: '/home/banner'
     })
 }
+
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const findNewAPI = () => {
+    return httpInstance({
+        url: '/home/new'
+    })
+}
+
+/**
+ * @description: 获取人气推荐
+ * @param {*}
+ * @return {*}
+ */
+export const findHotAPI = () => {
+    return httpInstance({
+        url: '/home/hot'
+    })
+}
--- src/views/Home/components/HomeHot.vue
+++ src/views/Home/components/HomeHot.vue
@@ -1,3 +1,69 @@
+<script setup>
+import { onMounted,ref } from 'vue';
+import HomePanel from './HomePanel.vue';
+import { findHotAPI } from '@/apis/home';
+
+// 获取数据
+const hotList = ref([])
+
+const getHotList = async () => {
+    const res = await findHotAPI()
+    hotList.value = res.result
+}
+
+onMounted(() => getHotList())
+</script>
+
 <template>
-    我是人气推荐
+    <HomePanel title="人气推荐" sub-title="人气爆款 不容错过">
+        <ul class="goods-list">
+            <li v-for="item in hotList" :key="hotList.id">
+                <RouterLink to="/">
+                    <img :src="item.picture" alt="" />
+                    <p class="name">{{ item.alt }}</p>
+                    <p class="price">&yen;{{ item.id }}</p>
+                </RouterLink>
+            </li>
+        </ul>
+    </HomePanel>
 </template>
+
+<style scoped lang="scss">
+.goods-list{
+    display: flex; // 设置列表为弹性布局子元素li会水平排列
+    justify-content: space-between;
+    height: 406px;
+
+    li{
+        width: 306px;
+        height: 406px;
+
+        background: #f0f9f4;
+        transition: all .5s; // 过度动画0.5秒平滑过渡
+
+        &:hover{
+            transform: translate3d(0, 3px, 0);
+            box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
+        }
+
+        img{
+            width: 306px;
+            height: 306px;
+        }
+
+        p{
+            font-size: 22px;
+            padding-top: 12px;
+            text-align: center;
+            text-overflow: ellipsis;
+            overflow: hidden;
+            white-space: nowrap;
+        }
+    }
+
+    .price{
+        color: $priceColor;
+    }
+
+}
+</style>
