aa3da42e956bc58ef84d9959d7c94ad1b00bca4d
YangDoo <2087058585@qq.com>
2025-08-07 21:32:48 +0800

    二级分类-面包屑导航实现和基础列表渲染

--- src/apis/category.js
+++ src/apis/category.js
@@ -1,10 +1,39 @@
-import request from '@/utils/http'
+import httpInstance from '@/utils/http'
 
 export function getCategoryAPI(id){
-    return request({
+    return httpInstance({
         url: '/category',
-        params: {
-            id
-        }
+        params: { id }
+    })
+}
+
+/**
+ * @description: 获取二级分类列表数据
+ * @param {*} id 分类id
+ * @return {*}
+ */
+
+export const getCategoryFilterAPI = (id) => {
+    return httpInstance({
+        url: '/category/sub/filter',
+        params: { id }
+    })
+}
+
+/**
+ * @description: 获取导航数据
+ * @data {
+    categoryID: 1005000, 
+    page: 1,
+    pageSize: 20,
+    sortField: 'publishTime' | 'orderNum' | 'evaluateNum'
+    }
+ * @return {*}
+ */
+export const getSubCategoryAPI = (data) => {
+    return httpInstance({
+        url: '/category/goods/temporary',
+        method: 'POST',
+        data
     })
 }
--- src/utils/http.js
+++ src/utils/http.js
@@ -12,8 +12,8 @@ httpInstance.interceptors.request.use(config => {
 }, e => Promise.reject(e))
 
 // axios响应式拦截器
-httpInstance.interceptors.response.use(res => res.data, e => {
-    return Promise.reject(e)
-})
+httpInstance.interceptors.response.use(
+    res => res.data,
+    e => Promise.reject(e))
 
 export default httpInstance
