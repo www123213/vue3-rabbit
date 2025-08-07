2d10d01d513fea41619a5772bf8e57a22fa85853
YangDoo <2087058585@qq.com>
2025-08-07 17:24:19 +0800

    二级分类-路由

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -31,10 +31,10 @@ const { bannerList } = useBanner()
        <div class="sub-list">
         <h3>全部分类</h3>
         <ul>
-          <li v-for="i in categoryData.children" :key="i.id">
-            <RouterLink :to="`/category/sub/${i.id}`">
-              <img :src="i.picture" />
-              <p>{{ i.name }}</p>
+          <li v-for="item in categoryData.children" :key="item.id">
+            <RouterLink :to="`/category/sub/${item.id}`">
+              <img :src="item.picture" />
+              <p>{{ item.name }}</p>
             </RouterLink>
           </li>
         </ul>
