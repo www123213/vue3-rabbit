5913f6ecfd3c70ee64e918e7b01d84dbf287483d
YangDoo <2087058585@qq.com>
2025-08-18 16:52:58 +0800

    详情页-图片预览组件-完结

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -1,13 +1,23 @@
 <script setup>
-import { ref } from 'vue'
+import { ref,watch } from 'vue'
+import { useMouseInElement } from '@vueuse/core'
+
+// props适配图片列表
+defineProps({
+  imageList: {
+    type: Array,
+    default: () => []
+  }
+})
+
 // 图片列表
-const imageList = [
-  "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
-  "https://yanxuan-item.nosdn.127.net/e801b9572f0b0c02a52952b01adab967.jpg",
-  "https://yanxuan-item.nosdn.127.net/b52c447ad472d51adbdde1a83f550ac2.jpg",
-  "https://yanxuan-item.nosdn.127.net/f93243224dc37674dfca5874fe089c60.jpg",
-  "https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg"
-]
+// const imageList = [
+//   "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
+//   "https://yanxuan-item.nosdn.127.net/e801b9572f0b0c02a52952b01adab967.jpg",
+//   "https://yanxuan-item.nosdn.127.net/b52c447ad472d51adbdde1a83f550ac2.jpg",
+//   "https://yanxuan-item.nosdn.127.net/f93243224dc37674dfca5874fe089c60.jpg",
+//   "https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg"
+// ]
 
 // 1. 小图切换大图显示
 const activeIndex = ref(0)
@@ -15,6 +25,44 @@ const activeIndex = ref(0)
 const enterhandler = (i) => {
     activeIndex.value = i
 }
+
+// 2. 获取鼠标相对位置
+const target = ref(null)
+const { elementX, elementY, isOutside } = useMouseInElement(target)
+
+// 3. 控制滑块跟随鼠标移动 (监听elementX/Y变化, 一旦变化 重新设置left/top)
+const left = ref(0)
+const top = ref(0)
+
+const positionX = ref(0)
+const positionY = ref(0)
+
+watch([elementX, elementY, isOutside], ()=>{
+  // 如果鼠标没有移入到盒子里面 直接不执行后面的逻辑
+  if(isOutside.value) return
+  // 有效范围内控制滑块距离
+  // 横向
+  if(elementX.value > 100 && elementX.value < 300){
+    left.value = elementX.value - 100
+  }
+  // 纵向
+  if(elementY.value > 100 && elementY.value < 300){
+    top.value = elementY.value - 100
+  }
+
+  // 处理边界
+  if(elementX.value > 300){ left.value = 200 }
+  if(elementX.value < 100){ left.value = 0 }
+
+  if(elementY.value > 300){ top.value = 200 }
+  if(elementY.value < 100){ top.value = 0 }
+
+  // 控制大图显示
+  positionX.value = -left.value * 2
+  positionY.value = -top.value * 2
+})
+
+
 </script>
 
 
@@ -25,7 +73,7 @@ const enterhandler = (i) => {
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div class="layer" v-show="!isOutside" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
@@ -36,11 +84,11 @@ const enterhandler = (i) => {
     <!-- 放大镜大图 -->
     <div class="large" :style="[
       {
-        backgroundImage: `url(${imageList[0]})`,
-        backgroundPositionX: `0px`,
-        backgroundPositionY: `0px`,
+        backgroundImage: `url(${imageList[activeIndex]})`,
+        backgroundPositionX: `${positionX}px`,
+        backgroundPositionY: `${positionY}px`,
       },
-    ]" v-show="false"></div>
+    ]" v-show="!isOutside"></div>
   </div>
 </template>
 
--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -27,7 +27,7 @@ onMounted(() => getGoods())
             -->
           <el-breadcrumb-item :to="{ path: `/category/${goods.categories[1].id}` }">{{ goods.categories[1].name }}</el-breadcrumb-item>
           <el-breadcrumb-item :to="{ path: `/category/sub/${goods.categories[0].id}` }">{{ goods.categories[0].name }}</el-breadcrumb-item>
-          <el-breadcrumb-item>抓绒保暖，毛毛虫子儿童运动鞋</el-breadcrumb-item>
+          <el-breadcrumb-item>{{ goods.name }}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
       <!-- 商品信息 -->
@@ -36,7 +36,7 @@ onMounted(() => getGoods())
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-              <ImageView />
+              <ImageView :image-list="goods.mainPictures"/>
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
